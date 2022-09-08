3d180403a8d62e8dabd0bcca107c96993b2d6ebe
Shane Reid <shane@ivodigital.com>
2022-09-08 13:29:25 +0200

    import prop types from wherevs

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
-import {
-  View,
-  requireNativeComponent,
-  ViewPropTypes
-} from 'react-native';
-import { bool, string, number, array, shape, arrayOf } from 'prop-types';
-import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+import React, { Component } from "react";
+import { ViewPropTypes } from "deprecated-react-native-prop-types";
+import { View, requireNativeComponent } from "react-native";
+import { bool, string, number, array, shape, arrayOf } from "prop-types";
+import resolveAssetSource from "react-native/Libraries/Image/resolveAssetSource";
 
 class ImageSequence extends Component {
   render() {
@@ -13,38 +10,39 @@ class ImageSequence extends Component {
 
     // reorder elements if start-index is different from 0 (beginning)
     if (this.props.startFrameIndex !== 0) {
-      normalized = [...normalized.slice(this.props.startFrameIndex), ...normalized.slice(0, this.props.startFrameIndex)];
+      normalized = [
+        ...normalized.slice(this.props.startFrameIndex),
+        ...normalized.slice(0, this.props.startFrameIndex),
+      ];
     }
 
-    return (
-      <RCTImageSequence
-        {...this.props}
-        images={normalized} />
-    );
+    return <RCTImageSequence {...this.props} images={normalized} />;
   }
 }
 
 ImageSequence.defaultProps = {
   startFrameIndex: 0,
   framesPerSecond: 24,
-  loop: true
+  loop: true,
 };
 
 ImageSequence.propTypes = {
   startFrameIndex: number,
   images: array.isRequired,
   framesPerSecond: number,
-  loop: bool
+  loop: bool,
 };
 
-const RCTImageSequence = requireNativeComponent('RCTImageSequence', {
+const RCTImageSequence = requireNativeComponent("RCTImageSequence", {
   propTypes: {
     ...ViewPropTypes,
-    images: arrayOf(shape({
-      uri: string.isRequired
-    })).isRequired,
+    images: arrayOf(
+      shape({
+        uri: string.isRequired,
+      })
+    ).isRequired,
     framesPerSecond: number,
-    loop: bool
+    loop: bool,
   },
 });
 
